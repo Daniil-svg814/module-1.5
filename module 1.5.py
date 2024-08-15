8ff77e821f9a7bdfababe8e5e38460f91d03bb61
Daniil-svg814 <abduhalikovdaniil@gmail.com>
2024-08-15 15:03:03 +0300

    Add files via upload

---
+++ module 1.5.py
@@ -0,0 +1,5 @@
+immutable_var = 1, 'a', 3, 'b'
+print(immutable_var)
+#после того как кортеж создан, в него нельзя добавлять элементы, а так же изменять и удалять
+immutable_var = 1,2,3,4
+print(immutable_var)
